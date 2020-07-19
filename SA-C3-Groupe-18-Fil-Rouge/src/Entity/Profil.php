f9dd7ee31bd9d3232d7f1f7586b0583fa76dee09
peinda <mamependa2011@gmail.com>
2020-07-19 23:50:32 +0000

    creation entité sprint1

---
+++ SA-C3-Groupe-18-Fil-Rouge/src/Entity/Profil.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProfilRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProfilRepository::class)
+ */
+class Profil
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $libell;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibell(): ?string
+    {
+        return $this->libell;
+    }
+
+    public function setLibell(string $libell): self
+    {
+        $this->libell = $libell;
+
+        return $this;
+    }
+}
